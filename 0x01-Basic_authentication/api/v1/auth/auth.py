adb4a167a61beb579deee4aec9e8a44099006bc9
Mark Manani <mikonani413@gmail.com>
2024-11-16 02:26:25 +0300

    Task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ from typing import List, TypeVar
 class Auth:
     """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Public method that returns False - path and excluded paths"""
+        """Determines which routes don't need authentication"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path[-1] != '/':
@@ -18,9 +18,11 @@ class Auth:
             return True
 
     def authorization_header(self, request=None) -> str:
-        """Public method that returns None - request will
-        be the Flask request object"""
-        return None
+        """Validate all requests to secure the API"""
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):  # type: ignore
         """Public that retuns None - request will be the
