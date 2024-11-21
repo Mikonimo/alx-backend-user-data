946d395830a1e5186837d45a1beb7ee6d0c6b030
mikonimo <mikonani413@gmail.com>
2024-11-22 01:42:34 +0300

    uuid func

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,6 +19,13 @@ def _hash_password(password: str) -> bytes:
     return hashpw(password.encode('utf-8'), gensalt())
 
 
+def _generate_uuid() -> str:
+    """ Generates UUID
+    Returns string representation of new UUID
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database
     """
