eba03ab9da87ab8c81673e65b88baa4457b71795
mikonimo <mikonani413@gmail.com>
2024-11-23 01:08:10 +0300

    adding err code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def logout() -> None:
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            return redirect(url_for('index'))
+            return redirect(url_for('index')), 302
     else:
         abort(403)
 
